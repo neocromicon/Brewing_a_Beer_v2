c0529dd9c15559261a6e82766d2c084bdbcc54b5
neocromicon <neocromicon@live.de>
2013-05-30 22:54:35 +0200

    Update

--- Base/BrewingBase.java
+++ Base/BrewingBase.java
@@ -1,12 +1,18 @@
 package mods.Brewing_a_Beer_v2.Base;
 
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "Brewing_a_Beer", name = "Brewing a Beer", version = "2.0")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false, 
+channels = {"BeerModChannel"}, packetHandler = PacketHandler.class)
 
 public class BrewingBase {
-	
+
 	@Instance
 	public static BrewingBase instance = new BrewingBase();
-	public static String modVersion = "1.4";
+	public static String modVersion = "2.0";
     public static String modID = "Brewing_a_Beer";
 
 }
