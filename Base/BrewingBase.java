834112bdcfbc9ac55b088bf3efe6c84da998474b
neocromicon <neocromicon@live.de>
2013-05-30 22:36:09 +0200

    Init

---
+++ Base/BrewingBase.java
@@ -0,0 +1,10 @@
+package mods.Brewing_a_Beer_v2.Base;
+
+import cpw.mods.fml.common.Mod.Instance;
+
+public class BrewingBase {
+	
+	@Instance
+	public static BrewingBase instance = new BrewingBase();
+
+}
