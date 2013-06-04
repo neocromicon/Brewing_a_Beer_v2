58b5d468df86282e02e6dfccd2995f1a0fb554c5
neocromicon <neocromicon@live.de>
2013-06-04 10:49:16 +0200

    Update
    Add "isProperty" not Testet

--- Handlers/PropertyHandler.java
+++ Handlers/PropertyHandler.java
@@ -1,9 +1,12 @@
 package mods.Brewing_a_Beer_v2.Handlers;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
 public class PropertyHandler {
+	
+	public static Properties props = new Properties();
 	
 	protected static String Crafting = "";
 	protected static String CraftingServer = "Brewing_a_Beer_Crafting_Settings_Server.cfg";
@@ -21,21 +24,29 @@ public class PropertyHandler {
 	}
 	
 	public static String getProperty(String File, String Store) {
-		try {
-			Properties props = new Properties();
+		try {			
 			String OpenFile = new StringBuilder().append(File).toString();
 			props.load(new FileInputStream(OpenFile));
 			String getProp = props.getProperty(Store);
 			return getProp;
-		} catch (Exception exception) {
-			System.err.println("Could not detect" + File);
-			exception.printStackTrace();
+		} catch (Exception ex) {
+			System.err.println("Could not detect: " + File);
+			ex.printStackTrace();
 			return null;
 		}
 	}
 	
-	public static boolean isProperty()
+	public static boolean isProperty(String File)
 	{
+		try {
+			File OpenFile = new File(File);
+			if (OpenFile.exists())
+			{ return true; }
+		} catch (Exception ex) {
+			System.err.println(File + " is not valid File or is missing");
+			ex.printStackTrace();
+			return false;
+		}
 		return false;		
 	}
 }
